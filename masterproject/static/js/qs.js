5e613d2cc764b5869cc6c24442c1568baf17bedd
Marte Løge <marte.loge@gmail.com>
2015-01-27 13:59:48 +0100

    Changed js

--- masterproject/static/js/qs.js
+++ masterproject/static/js/qs.js
@@ -1,27 +1,138 @@
 var qs = qs || {};
-qs.bindForm = function(bindings, id) {
+qs.bindForm = function(bindings, id, src1, src2) {
   
   var createHandler = function(key, value) {
     $("#"+key).click(function() { // klikker på bilde
       $("#" + id).val(value);
-      $("div#content").find('img').removeClass('selected');
+
+      var src = $(this).attr('src');
+      var src_selected = src.replace('.svg', '-selected.svg');
+
+      //fjerner alle klasser og setter src til svart ikon
+      $("#content-module").find('img').removeClass('selected');
+
+      //Legger til klassen selected på bildet som ble klikket på
       $(this).addClass('selected');
+
+      //Endrer valgt bilde til grønt
+      $(this).attr("src",src_selected);
+
+      //Fader ut skjermen
+      $( "#wrapper" ).fadeOut( "slow", function() {
+        $("#btn-post").click();
+      });
     });
   };
   for (key in bindings) {
     createHandler(key, bindings[key]);
   }
+};
+
+function addPattern() {
+  var lock = new PatternLock('#patternContainer', {
+    onDraw:function(pattern){
+      pattern = lock.getPattern();
+      if(pattern.length>=4){
+        $('#retrypattern').click(function() {
+          lock.reset();
+        });
+        $('#submitpattern').click(function() {
+          $('#id_sequence').val(pattern);
+          $('#btn-post').click();
+        });
+        $('#retrypattern_post').click(function() {
+          $('#id_sequence').val(pattern);
+          $('#btn-post').click();
+        });
+        $('#continue').click(function() {
+          $('#id_sequence').val(pattern);
+          $('#btn-post').click();
+          window.location.replace("patterninformation");
+        });
+      }
+      else{
+        lock.error();
+        $('#retrypattern').click(function() {
+          lock.reset();
+        });
+      }
+    },
+    radius: 80,
+    margin: 30,
+  });
+};
+
+//AndroidOS, BlackBerryOS, PalmOS, SymbianOS, WindowsMobileOS, WindowsPhoneOS, iOS, MeeGoOS, MaemoOS, JavaOS, webOS, badaOS, BREWOS
+function getMobileOperatingSystem() {
+    var md = new MobileDetect(window.navigator.userAgent);
+    var os = md.os();
+    $('#os_notfound').hide();
+    $('#os_found').show();
+
+    if(os==null){
+      $('#os_notfound').show();
+      $('#os_found').hide();
+      $('.dropdown').dropdown({
+        onChange: function(val) {
+          $('#id_mobileOS').val(val);  
+        }
+      });
+      $('#submitOS').click(function() {
+        $('#btn-post').click();
+      });
+    }
+    else if(os.match("iOS")) {
+      $('img#yes').attr('id', 'ios');
+      $('img#no').attr('id', 'no_ios');
+      $('img#unknown').attr('id', 'unknown_ios');
+      $('img#mobileOS').attr('src', '/static/images/icons/ios.svg');
+    }
+    else if(os.match("Android")) {
+      $('img#yes').attr('id', 'android');
+      $('img#no').attr('id', 'no_android');
+      $('img#unknown').attr('id', 'unknown_android');
+      $('img#mobileOS').attr('src', '/static/images/icons/android.svg');
+    }
+    else if(os.match("BlackBerry")) {
+      $('img#yes').attr('id', 'blackberry');
+      $('img#no').attr('id', 'no_blackberry');
+      $('img#unknown').attr('id', 'unknown_blackberry');
+      $('img#mobileOS').attr('src', '/static/images/icons/blackberry.svg');
+    }
+    else if(os.match("Windows")){
+      $('img#yes').attr('id', 'windows');
+      $('img#no').attr('id', 'no_windows');
+      $('img#unknown').attr('id', 'unknown_windows');
+      $('img#mobileOS').attr('src', '/static/images/icons/windows.svg');
+    }
+    else {
+      $('#os_notfound').show();
+      $('#os_found').hide();
+      $('.dropdown').dropdown({
+        onChange: function(val) {
+          $('#id_mobileOS').val(val);  
+        }
+      });
+      $('#submitOS').click(function() {
+        $('#btn-post').click();
+      });
+    }
+};
+
+function getDevice() {
+  var md = new MobileDetect(window.navigator.userAgent);
+  if(md.phone() || md.mobile()){}
+  else if(md.tablet()){
+    window.location.replace("/nomobile");
+  }
+  else {
+    window.location.replace("/nomobile");
+  }
 
 };
 
-var populateNations = function(divId) {
-      $select = $("#" + divId);
-      
-      for (var i = 0; i < nations.length; ++i) {
-        $select.append($("<option></option>")
-          .attr("value",nations[i].code)
-          .text(nations[i].name)); 
-      }
-    };
-
-
+function addAge() {
+  $("#age").keyup(function(){
+    $("#id_age").val($("#age").val());
+  });
+}
